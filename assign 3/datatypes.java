ae989523d530d7f185980a968de07b1bf7c724f5
ShripadCoolkarni <shripadk68@gmail.com>
2023-06-03 21:26:09 +0530

    all assignments  till 2 june

---
+++ assign 3/datatypes.java
@@ -0,0 +1,16 @@
+class SecBigArr{
+	public static void main(String args [])
+	{
+	 int a;
+	 int []rno= new int []{21,12,11,53,44,2};
+	 rno.sort();
+	 for(i=0;i<rno.length;i++)
+	 {
+		 System.out.pritln("Given array is :- ");
+		 System.out.println(rno[i]);
+	 }
+		
+       }
+	
+	
+}
